d55bdd2c8bd8dd954c2f02fdc968ceb3de767c0f
denis-marul <marul2na3@gmail.com>
2023-07-23 05:27:53 +0300

    hwnew
    hwnew

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -2,26 +2,18 @@ import { Section } from 'components/Section/Section';
 import { Form } from 'components/Form/Form';
 import { ContactsList } from 'components/ContactsList/ContactsList';
 import { Filter } from 'components/Filter/Filter';
-import { useSelector } from 'react-redux';
-import { getContacts } from 'redux/contacts/selectors';
 import css from './Contacts.module.css';
 
 const Contacts = () => {
-  const contacts = useSelector(getContacts);
   return (
     <div className={css.container}>
       <Section title={'Phonebook'}>
         <Form />
       </Section>
-
-      {contacts.length > 0 ? (
-        <Section title={'Contacts'}>
-          <Filter />
-          <ContactsList />
-        </Section>
-      ) : (
-        <p>You have no contacts yet.</p>
-      )}
+      <Section title={'Contacts'}>
+        <Filter />
+        <ContactsList />
+      </Section>
     </div>
   );
 };
